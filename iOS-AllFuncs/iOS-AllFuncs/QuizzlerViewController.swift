eb5b167a286e76b029bb15e0b95a2b21e534dba9
Dimitar-LDimitrov <dimitar.ldimitrovv@gmail.com>
2023-05-19 17:25:33 +0300

    init

--- iOS-AllFuncs/iOS-AllFuncs/QuizzlerViewController.swift
+++ iOS-AllFuncs/iOS-AllFuncs/QuizzlerViewController.swift
@@ -9,25 +9,32 @@ class QuizzlerViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    var quiz = [String]()
-    var showedQuestions = [Int]()
+    var quiz = [[String]]()
+    var showedQuestions = Set<Int>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        var arr = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+        print(arr[2][2])
+ 
         quiz = [
-            "A slug's blood is green.",
-            "Approximately one quarter of human bones are in the feet.",
-            "The total surface area of two human lungs is approximately 70 square metres.",
-            "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.",
-            "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.",
-            "It is illegal to pee in the Ocean in Portugal.",
-            "You can lead a cow down stairs but not up stairs.",
-            "Google was originally called 'Backrub'.",
-            "Buzz Aldrin's mother's maiden name was 'Moon'.",
-            "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.",
-            "No piece of square dry paper can be folded in half more than 7 times.",
-            "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog."
+           ["A slug's blood is green.", "True"],
+           ["Approximately one quarter of human bones are in the feet.", "True"],
+           ["The total surface area of two human lungs is approximately 70 square metres.", "True"],
+           ["In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take], it home to eat.", "True"],
+           ["In London, UK, if you happen to die in the House of Parliament, you are technically entitled], to a state funeral, because the building is considered too sacred a place.", "False"],
+           ["It is illegal to pee in the Ocean in Portugal.", "True"],
+           ["You can lead a cow down stairs but not up stairs.", "False"],
+           ["Google was originally called 'Backrub'.", "True"],
+           ["Buzz Aldrin's mother's maiden name was 'Moon'.", "True"],
+           ["The loudest sound produced by any animal is 188 decibels. That animal is the African], Elephant.", "False"],
+           ["No piece of square dry paper can be folded in half more than 7 times.", "False"],
+           ["Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small], dog.", "True"],
         ]
         
     }
@@ -35,18 +42,17 @@ class QuizzlerViewController: UIViewController {
     @IBAction func answerGiven(_ sender: UIButton) {
         
         var positionOfQuestion = Int.random(in: 0..<quiz.count)
+        var userAnswer = sender.titleLabel?.text
+        var questionAnswer = quiz[positionOfQuestion][1]
         
-        if showedQuestions.contains(positionOfQuestion) == false {
-            questionText.text = quiz[positionOfQuestion]
+        if  userAnswer == questionAnswer {
+            label.text = "Correct!"
         } else {
-            // да се направи логика, ако текущия въпрос вече е показван как
-            // със сигурност да се покаже такъв какъвто не е показван досега
+            label.text = "Wrong"
         }
         
-        showedQuestions.append(positionOfQuestion)
+        questionText.text = quiz[positionOfQuestion][0]
+        
+        showedQuestions.insert(positionOfQuestion)
     }
-}
-
-struct Question {
-    
 }
