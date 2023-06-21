4694c4b918fe9a10f7956cdeea3c3ab0f64a6869
Dimitar-LDimitrov <81306670+Dimitar-LDimitrov@users.noreply.github.com>
2023-06-21 10:25:17 +0300

    fetch the live data received from API

---
+++ iOS-AllFuncs/iOS-AllFuncs/Config.swift
@@ -0,0 +1,8 @@
+//
+//  Config.swift
+//  iOS-AllFuncs
+//
+//  Created by Dimitar Dimitrov on 20.06.23.
+//
+
+import Foundation
--- iOS-AllFuncs/iOS-AllFuncs/Controller/ClimaApp/ClimaViewController.swift
+++ iOS-AllFuncs/iOS-AllFuncs/Controller/ClimaApp/ClimaViewController.swift
@@ -1,8 +1,47 @@
 import UIKit
 
-class ClimaViewController: UIViewController {
+class ClimaViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // create a delegate for searchTextField
+        searchTextField.delegate = self
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        // remove keyboard when search is pressed
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    // enable return button on keyboard to be active when is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Can`t be empty"
+            return false
+        }
+    }
+    
+    // remove already typed City when pressed search/return button
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        searchTextField.text = ""
     }
 }
