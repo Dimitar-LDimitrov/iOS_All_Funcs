277792c0c22dc04e5538c47c1f7cfb85432b52ba
Dimitar-LDimitrov <81306670+Dimitar-LDimitrov@users.noreply.github.com>
2023-06-26 14:27:43 +0300

    correct mistakes from refactoring

--- iOS-AllFuncs/iOS-AllFuncs/Models/ClimaApp/WeatherManager.swift
+++ iOS-AllFuncs/iOS-AllFuncs/Models/ClimaApp/WeatherManager.swift
@@ -31,13 +31,19 @@ struct WeatherManager {
     
     // Networking(fetch the live data)
     func performRequest(with urlString: String) {
-        // 1. Create a URL
         if let url = URL(string: urlString) {
-            // 2. Create a URL Session
             let session = URLSession(configuration: .default)
-            // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: parseJSON(data:response:error:))
-            // 4. Start the task
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let weather = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
             task.resume()
         }
     }
