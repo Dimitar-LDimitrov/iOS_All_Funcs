32e55e1ea4c53987f9a147e8a98ecccb685d7f26
Dimitar-LDimitrov <81306670+Dimitar-LDimitrov@users.noreply.github.com>
2023-06-29 16:19:53 +0300

    integrate cocoapods to project and add new pod called CLTypingLabel

--- iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/FlashChatViewController.swift
+++ iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/FlashChatViewController.swift
@@ -8,22 +8,35 @@
 import UIKit
 
 class FlashChatViewController: UIViewController {
-
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let titleLetters = titleLabel.text
+        var charIndex = 0.0
+        titleLabel.text = ""
+        if let titleText = titleLetters {
+            for letter in titleText {
+                
+                Timer.scheduledTimer(withTimeInterval: 0.13 * charIndex, repeats: false) { (timer) in
+                    self.titleLabel.text?.append(letter)
+                }
+                charIndex += 1
+            }
+        }
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
