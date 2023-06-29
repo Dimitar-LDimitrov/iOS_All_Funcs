dc82a5e5a5b1926703636708ffacb8ef934e1a5a
Dimitar-LDimitrov <81306670+Dimitar-LDimitrov@users.noreply.github.com>
2023-06-29 16:55:19 +0300

    animate some texts using 3rd party library subclass of UILabel

--- iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/FlashChatViewController.swift
+++ iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/FlashChatViewController.swift
@@ -6,37 +6,15 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class FlashChatViewController: UIViewController {
     
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let titleLetters = titleLabel.text
-        var charIndex = 0.0
-        titleLabel.text = ""
-        if let titleText = titleLetters {
-            for letter in titleText {
-                
-                Timer.scheduledTimer(withTimeInterval: 0.13 * charIndex, repeats: false) { (timer) in
-                    self.titleLabel.text?.append(letter)
-                }
-                charIndex += 1
-            }
-        }
+        titleLabel.text = "⚡️FlashChat"
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
