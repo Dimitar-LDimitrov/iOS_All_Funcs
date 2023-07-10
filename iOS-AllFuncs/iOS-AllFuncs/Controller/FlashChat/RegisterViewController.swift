d1f7c3faae866720c10db4206096058f4a4ddb13
Dimitar-LDimitrov <81306670+Dimitar-LDimitrov@users.noreply.github.com>
2023-07-10 16:39:40 +0300

    Create Podfile and add Firebase pods for authentication

--- iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/RegisterViewController.swift
+++ iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/RegisterViewController.swift
@@ -1,29 +1,21 @@
-//
-//  RegisterViewController.swift
-//  iOS-AllFuncs
-//
-//  Created by Dimitar Dimitrov on 28.06.23.
-//
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+              
+            }
+        }
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
