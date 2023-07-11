f5c4e2573d64baa21d71b2c0a7734da2b3edeb78
Dimitar-LDimitrov <81306670+Dimitar-LDimitrov@users.noreply.github.com>
2023-07-11 16:46:39 +0300

    create extension for TableViewDataSource and  model for Messages

--- iOS-AllFuncs/iOS-AllFuncs/Constants.swift
+++ iOS-AllFuncs/iOS-AllFuncs/Constants.swift
@@ -4,4 +4,5 @@ struct Constants {
     static let registerSegue = "RegisterToChat"
     static let loginSegue = "LogInToChat"
     static let flashChatTitle = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
 }
--- iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/ChatViewController.swift
+++ iOS-AllFuncs/iOS-AllFuncs/Controller/FlashChat/ChatViewController.swift
@@ -3,8 +3,20 @@ import FirebaseAuth
 
 class ChatViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTextField: UITextField!
+    
+    var messages: [Message] = [
+        Message(sender: "mitko@abv.bg", body: "Hey!"),
+        Message(sender: "dada@dada.by", body: "Hello"),
+        Message(sender: "mitko@abv.bg", body: "What`s up?")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         
         navigationItem.hidesBackButton = true
         navigationItem.title = "⚡️FlashChat"
@@ -20,5 +32,25 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
             print("Error signing out: %@", signOutError)
         }
+    }
+}
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].body
+        
+        return cell
+    }
+}
+
+// just to practice delegates print on consol the number of selected row
+extension ChatViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
     }
 }
